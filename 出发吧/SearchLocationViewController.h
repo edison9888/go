bba4a90949a45017d2e4e0d7b50eefc7750e2819
oasisfans <11473124@qq.com>
2013-01-26 15:38:52 +0800

    Test github gui client

--- 出发吧/SearchLocationViewController.h
+++ 出发吧/SearchLocationViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 //@class SearchLocationDataController;
+//test github gui client
 
 @interface SearchLocationViewController : UITableViewController <UISearchBarDelegate>
 {
